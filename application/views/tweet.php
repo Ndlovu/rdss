b58d3691bcbca60fe02f36e10bf37bbf50668dc8
Ndlovu <patrickngachuru@gmail.com>
2016-05-08 16:27:13 +0300

    Tweet modification

--- application/views/tweet.php
+++ application/views/tweet.php
@@ -1,22 +1,23 @@
             <?php require_once("includes/header.php"); ?>
 
-            <a class="twitter-timeline"  href="https://twitter.com/hashtag/cholera" data-widget-id="714334051201650688">Tweets</a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            <a class="twitter-timeline"  href="https://twitter.com/search?q=cholera" data-widget-id="714334051201650688">Tweets about cholera</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
+         <a class="twitter-timeline"  href="https://twitter.com/search?q=measles" data-widget-id="728932782609715200">Tweets about measles</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
 
 
-
-                     <a class="twitter-timeline"  href="https://twitter.com/hashtag/typhoid" data-widget-id="714334051201650688"></a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-
-                     <a class="twitter-timeline"  href="https://twitter.com/hashtag/menengitis" data-widget-id="714334051201650688"></a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <a class="twitter-timeline"  href="https://twitter.com/search?q=typhoid" data-widget-id="728928351033937920">Tweets about typhoid</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
+          
 
 
-
-                     <a class="twitter-timeline"  href="https://twitter.com/hashtag/hepatitis" data-widget-id="714334051201650688"></a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
+    <a class="twitter-timeline"  href="https://twitter.com/search?q=hepatitis" data-widget-id="728934067673174016">Tweets about hepatitis</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
+          
 
                     
           <?php require_once("includes/footer.php"); ?>
